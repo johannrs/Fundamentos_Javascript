4c6f34e1667f6ca37f7c311fd05bfa7a031eb074
JohannRios <johannr.dev@gmail.com>
2021-03-26 15:41:03 -0500

    cambio 1

--- clase2.js
+++ clase2.js
@@ -1,11 +1,11 @@
 var nombre = 'Sacha', apellido = 'Lifszyc'
 
 var nombreEnMayusculas = nombre.toUpperCase()
-var apellidoEnMinusculas = apellido.toLowerCase()
+// var apellidoEnMinusculas = apellido.toLowerCase()
 
-var primeraLetraDelNombre = nombre.charAt(0)
-var cantidadDeLetrasDelNombre = nombre.length
+// var primeraLetraDelNombre = nombre.charAt(0)
+// var cantidadDeLetrasDelNombre = nombre.length
 
-var nombreCompleto = `${nombre} ${apellido.toUpperCase()}`
+// var nombreCompleto = `${nombre} ${apellido.toUpperCase()}`
 
-var str = nombre.substr(1, 2)
+// var str = nombre.substr(1, 2)
